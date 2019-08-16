de725cfe182f5f15596c5f7c4cb610d042824338
paulniu <niupuyue@aliyun.com>
2019-08-16 16:33:36 +0800

    循环

--- 11_cycle.py
+++ 11_cycle.py
@@ -46,13 +46,77 @@ for i in range(10):
     print("当前的数字是%d" % i)
 
 # 同样我们可以指定遍历数字列表的范围
-for ii in range(2,6):
+for ii in range(2, 6):
     print("当前的数字是%d" % ii)
 
 # 我们可以指定遍历数字列表的步长
-for iii in range(1,20,3):
+for iii in range(1, 20, 3):
     print("当前的数字是%d" % iii)
 
+# 我们还可以使用负数
+for iiii in range(-10, -100, -30):
+    print("当前数字是%d" % iiii)
 
+# 我们可以结合range()和len()函数便利一个序列的索引
+a = ["google", "baidu", "Taobao", "Tencent"]
+for temp in range(len(a)):
+    print(temp, a[temp])
 
+# 同时我们还可以使用range()函数来创建一个列表
+print(list(range(5)))
 
+'''
+break语句可以跳出for和while循环体，如果我们从for或者while循环中终止，任何对应的循环else块将不再执行
+'''
+for charr in 'paulniu':
+    if charr == 'l':
+        break
+    print("当前字符是%s" % charr)
+
+'''
+continue语句被用来告诉苏Python跳过当前循环块中剩余的语句，然后继续进行下一个循环
+'''
+for letter in "paulniu":
+    if letter == "i":
+        continue
+    print("当前的字符串是=" + letter)
+
+'''
+pass语句在Python是空语句，是为了保持程序结构的完整性
+pass不做任何事情，一般做占位符语句
+'''
+
+# 加餐
+
+'''
+//////////////////////////////////////////////////////////////////////////////////////////////////
+使用内置enumerate函数进行遍历
+'''
+sequence = [12, 34, 56, 23, 34, 45, 56]
+for i, j in enumerate(sequence):
+    print(i, j)
+
+'''
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+for循环1-100内所有证书的和
+'''
+n = 0
+sum = 0
+for n in range(0, 101):
+    sum += n
+print("结果是%d" % sum)
+
+'''
+////////////////////////////////////////////////////////////////
+使用循环嵌套实现99乘法法则
+'''
+start = 1
+while start <= 9:
+    sec = 1
+    while sec <= start:
+        mut = start * sec
+        print("%d x %d = %d" % (start, sec, mut), end=" ")
+        sec += 1
+    print("")
+    print("")
+    start += 1
